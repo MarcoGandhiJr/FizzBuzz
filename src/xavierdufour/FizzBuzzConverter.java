b2f8a6232ea04990cf9f2dc6953860d89d92a430
Xavier Dufour-Maheux <xavierdufourmaheux@gmail.com>
2020-09-30 18:04:24 -0400

    Completion de la methode convert appartenant à la classe FizzBuzzConverter

--- src/xavierdufour/FizzBuzzConverter.java
+++ src/xavierdufour/FizzBuzzConverter.java
@@ -3,6 +3,19 @@ package xavierdufour;
 public class FizzBuzzConverter {
 
     public String convert(int number) {
-        return "";
+        String answer = String.valueOf(number);
+        if (number % 3 == 0) {
+            answer = "Fizz";
+        }
+
+        if (number % 5 == 0) {
+            if (number % 3 == 0) {
+                answer += " Buzz";
+                return answer;
+            }
+            answer = "Buzz";
+        }
+
+        return answer;
     }
 }
